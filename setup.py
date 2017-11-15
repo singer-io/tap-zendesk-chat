eee15ee62ec03316b3a515506b97d111411f1e0c
Christopher Merrick <chris@stitchdata.com>
2017-11-15 09:51:14 -0500

    version 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-zendesk-chat",
-      version="0.1.5",
+      version="0.1.6",
       description="Singer.io tap for extracting data from the Zendesk Chat API",
       author="Stitch",
       url="http://singer.io",
