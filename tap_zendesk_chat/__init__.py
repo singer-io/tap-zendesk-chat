0c62c0827b4fae454e7879b132a0e8ecb26a9470
VishalP <20889199+Vi6hal@users.noreply.github.com>
2022-10-03 11:14:28 +0530

    updated init for tap

--- tap_zendesk_chat/__init__.py
+++ tap_zendesk_chat/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import singer
-from singer.utils import parse_args
+from singer.utils import parse_args,handle_top_exception
 from singer.catalog import Catalog
 from .context import Context
 from .discover import discover
@@ -10,23 +10,17 @@ from .sync import sync
 REQUIRED_CONFIG_KEYS = ["start_date", "access_token"]
 LOGGER = singer.get_logger()
 
-def main_impl():
+
+@handle_top_exception(LOGGER)
+def main():
+    """performs sync and discovery."""
     args = parse_args(REQUIRED_CONFIG_KEYS)
     if args.discover:
         discover(args.config).dump()
-        print()
     else:
-        catalog = Catalog.from_dict(args.properties) \
-            if args.properties else discover(args.config)
-        ctx = Context(args.config, args.state, catalog)
+        ctx = Context(args.config, args.state, args.catalog or discover(args.config))
         sync(ctx)
 
-def main():
-    try:
-        main_impl()
-    except Exception as exc:
-        LOGGER.critical(exc)
-        raise exc
 
 if __name__ == "__main__":
     main()
