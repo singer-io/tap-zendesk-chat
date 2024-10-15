03ba797dab07cb9ecb69ce495e50bdac2a147958
Vi6hal <20889199+Vi6hal@users.noreply.github.com>
2024-10-15 11:33:00 +0000

    changed tap-impl for support to subdomain

--- tap_zendesk_chat/http.py
+++ tap_zendesk_chat/http.py
@@ -17,13 +17,18 @@ class Client:
         self.headers = {}
         self.headers["Authorization"] = f"Bearer {self.access_token}"
         self.headers["User-Agent"] = self.user_agent
+        if "subdomain" in config:
+            self.base_url = f"{config['subdomain']}.zendesk.com"
+        else:
+            self.base_url =  BASE_URL
+            LOGGER.warning("Missing Subdomain, please recheck the configuration")
         self.session = requests.Session()
 
     @backoff.on_exception(backoff.expo, RateLimitException, max_tries=10, factor=2)
     def request(self, tap_stream_id, params=None, url=None, url_extra=""):
         with metrics.http_request_timer(tap_stream_id) as timer:
 
-            url = url or f"{BASE_URL}/api/v2/{tap_stream_id}{url_extra}"
+            url = url or f"{self.base_url}/api/v2/{tap_stream_id}{url_extra}"
             LOGGER.info("calling %s %s", url, params)
             response = self.session.get(url, headers=self.headers, params=params)
             timer.tags[metrics.Tag.http_status_code] = response.status_code
