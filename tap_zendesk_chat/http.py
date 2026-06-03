5231c8e933677182f3037ac2011d63f96feba716
Leslie VanDeMark <lvandemark@talend.com>
2026-06-03 20:00:02 +0000

    make pylint happy

--- tap_zendesk_chat/http.py
+++ tap_zendesk_chat/http.py
@@ -66,7 +66,8 @@ class Client:
         elif response.status_code == 400:
             LOGGER.warning(
                 "The amount of data present for in %s stream is huge,\
-                The api has a pagination limit of 251 pages, please reduce the search window for this stream"
+                The api has a pagination limit of 251 pages, please reduce the search window for this stream",
+                tap_stream_id
             )
         response.raise_for_status()
         return response.json()
